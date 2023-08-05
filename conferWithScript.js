8f06e2a65f8c21839ea1e71ecc56a7099a9a4abf
Sergei Stekh <sergei.stekh@wundermanthompson.com>
2023-08-05 12:39:51 +0200

    added script to upload and test

---
+++ conferWithScript.js
@@ -0,0 +1,4 @@
+console.log('conferwithTrackingInvoiceId', conferwithTrackingInvoiceId);
+console.log('invoiceDate', invoiceDate);
+console.log('invoiceValue', invoiceValue);
+console.log('products', products);
